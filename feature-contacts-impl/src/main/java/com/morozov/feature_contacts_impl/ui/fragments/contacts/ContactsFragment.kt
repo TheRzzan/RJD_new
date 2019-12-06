9ce37c14ee9d0a4cb6d5954a0b47e279fc9ef7da
Yevgeniy Morozov <morozov.e@edu.spbstu.ru>
2019-12-06 19:42:07 +0300

    Contacts fragment created

--- feature-contacts-impl/src/main/java/com/morozov/feature_contacts_impl/ui/fragments/contacts/ContactsFragment.kt
+++ feature-contacts-impl/src/main/java/com/morozov/feature_contacts_impl/ui/fragments/contacts/ContactsFragment.kt
@@ -1,4 +1,102 @@
 package com.morozov.feature_contacts_impl.ui.fragments.contacts
 
-class ContactsFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.morozov.feature_contacts_impl.R
+import com.morozov.feature_contacts_impl.ui.adapters.contacts.ContactsAdapter
+import com.morozov.feature_contacts_impl.ui.adapters.item_touch_helper.ItemTouchHelperClass
+import com.morozov.feature_contacts_impl.ui.adapters.listeners.OnItemClickListener
+import kotlinx.android.synthetic.main.fragment_contacts_list.*
+
+class ContactsFragment: Fragment(), OnItemClickListener {
+
+    private lateinit var viewModel: ContactsViewModel
+
+    lateinit var adapter: ContactsAdapter
+    lateinit var itemTouchHelper: ItemTouchHelper
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflater.inflate(R.layout.fragment_contacts_list, container, false)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = activity?.run {
+            ViewModelProviders.of(this)[ContactsViewModel::class.java]
+        } ?: throw Exception("Invalid Activity")
+
+        viewModel.getContacts().observeForever {
+            when (it.command) {
+                ContactsViewModel.ViewCommand.Command.UPDATE_OLD_LIST -> {
+                    adapter.notifyDataSetChanged()
+                }
+                ContactsViewModel.ViewCommand.Command.SHOW_NEW_LIST -> {
+                    it.data?.let { it1 -> adapter.setData(it1) }
+                }
+            }
+
+            val colorPrimary = context?.resources?.getColor(R.color.colorPrimary)
+            val colorSecondary = context?.resources?.getColor(R.color.secondary_text)
+
+            colorSecondary?.let { it1 -> textFriends.setTextColor(it1) }
+            colorSecondary?.let { it1 -> textColleagues.setTextColor(it1) }
+            colorSecondary?.let { it1 -> textAll.setTextColor(it1) }
+
+            when (it.personLabel) {
+                ContactsViewModel.ViewCommand.PersonLabel.FRIEND -> {
+                    colorPrimary?.let { it1 -> textFriends.setTextColor(it1) }
+                }
+                ContactsViewModel.ViewCommand.PersonLabel.COLLEAGUE -> {
+                    colorPrimary?.let { it1 -> textColleagues.setTextColor(it1) }
+                }
+                ContactsViewModel.ViewCommand.PersonLabel.ALL -> {
+                    colorPrimary?.let { it1 -> textAll.setTextColor(it1) }
+                }
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        textAll.setOnClickListener {
+            viewModel.showAll()
+        }
+        textFriends.setOnClickListener {
+            viewModel.showFriends()
+        }
+        textColleagues.setOnClickListener {
+            viewModel.showColleagues()
+        }
+
+        adapter = ContactsAdapter(this, viewModel, relativeContacts)
+        recyclerContacts.adapter = adapter
+        recyclerContacts.layoutManager = LinearLayoutManager(context)
+
+        val callback = ItemTouchHelperClass(adapter)
+        itemTouchHelper = ItemTouchHelper(callback)
+        itemTouchHelper.attachToRecyclerView(recyclerContacts)
+
+        buttonAdd.setOnClickListener {
+
+        }
+
+        buttonFriend.setOnClickListener {
+
+        }
+
+        buttonColleague.setOnClickListener {
+
+        }
+    }
+
+    // OnItemClickListener impl
+    override fun onItemClick(view: View?, position: Int) {
+
+    }
 }
